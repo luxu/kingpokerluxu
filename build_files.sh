8415f3921a850ccc41b1b78012bbca0a7bad307d
Luciano Martins <zicadopv@gmail.com>
2024-04-08 11:14:30 -0300

    fix version pip

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Building project packages..."
-python3 -m pip3 install --upgrade pip
+#python3 -m pip3 install --upgrade pip
 
 echo "Building project packages..."
-python3 -m pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Migrating Database..."
-python3 manage.py migrate --noinput
+#python3 manage.py migrate --noinput
