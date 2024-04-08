0cc18c01fbd02d56c2d2c25d95cc8faed0dadc6d
Luciano Martins <zicadopv@gmail.com>
2024-04-08 11:13:35 -0300

    fix version pip

--- kingpoker/build_files.sh
+++ kingpoker/build_files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Building project packages..."
-python3 -m pip3 install --upgrade pip
+#python3 -m pip3 install --upgrade pip
 
 echo "Building project packages..."
-python3 -m pip3 install -q -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Migrating Database..."
-python3 manage.py migrate --noinput
+#python3 manage.py migrate --noinput
